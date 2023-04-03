8ac1a264b10eba05091fcbb809e864f10e9365dd
HOUDA LALAOUI MOUTAOUAKKIL <houdalalaoui115@gmail.com>
2023-04-03 13:02:42 +0000

    testing with mockito java

---
+++ app/src/main/java/com/example/testing_with_mockito_java/CalculatorService.java
@@ -0,0 +1,8 @@
+package com.example.testing_with_mockito_java;
+
+public interface CalculatorService {
+    public double add(double input1, double input2);
+    public double sous(double input1, double input2);
+    public double multi(double input1, double input2);
+    public double div(double input1, double input2);
+}
